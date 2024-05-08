b30f9dd35e26331992994f2250b48d60df98c59b
Mathias Ranna <46851862+MathiasRanna@users.noreply.github.com>
2024-05-08 14:53:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Changes maked agaaai
+          Changes maked last
         </a>
       </header>
     </div>
