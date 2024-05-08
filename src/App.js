7ff481913047cf8cb4af2e045951f6f5e44cd15a
Mathias Ranna <46851862+MathiasRanna@users.noreply.github.com>
2024-05-08 11:11:23 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React perfectly good
+          Learn React perfectly
         </a>
       </header>
     </div>
